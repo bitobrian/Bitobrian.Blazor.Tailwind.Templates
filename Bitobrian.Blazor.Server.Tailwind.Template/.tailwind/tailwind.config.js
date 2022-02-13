f863144c77377ec467a68db63e277a0043a37784
Brian Donaldson <brian@bitobrian.com>
2022-02-13 00:20:05 -0600

    init blazor with tailwind css and font awesome.

---
+++ Bitobrian.Blazor.Server.Tailwind.Template/.tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './**/*.html',
+        './**/*.razor',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Helvetica', 'Arial', 'sans-serif']
+            }
+        },
+    },
+    plugins: [],
+}
